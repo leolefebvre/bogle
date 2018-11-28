33148258dac760844c746744b88d8c59c4231a59
troleoleo <leolefebvre.contact@gmail.com>
2018-11-28 21:02:14 +0200

    Add utils scripts

---
+++ Unity/Bogle/Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+// ceci est un commentaire inutilie
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance != null)
+            {
+                return _instance;
+            }
+            _instance = FindObjectOfType<T>();
+            if (_instance == null)
+            {
+                //Debug.LogError("Singleton <" + typeof(T) + "> does not exists in the hierarchy.");
+            }
+            return _instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else
+        {//youbeach 
+            if (!Application.isEditor)
+            {
+                Debug.LogError(this + " You have two singletons of the same type in the hierarchy");
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}

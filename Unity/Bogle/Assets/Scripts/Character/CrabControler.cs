b750c4e70b0dff764a4d99304e835ee5367540d7
troleoleo <leolefebvre.contact@gmail.com>
2018-12-01 20:45:26 +0200

    Reorganise project hierarchy

--- Unity/Bogle/Assets/Scripts/Character/CrabControler.cs
+++ Unity/Bogle/Assets/Scripts/Character/CrabControler.cs
@@ -9,13 +9,28 @@ public enum MovingStates
     rotating = 2
 }
 
-public class TestCharacterControler : MonoBehaviour
+public class CrabControler : MonoBehaviour
 {
+    #region externalParameters
+
     [Header("Characters Parameters")]
     public float walkingSpeed = 5.0f;
     public float rotatingSpeed = 5.0f;
 
-    public Vector3 rotationAxis = Vector3.up;
+    public Vector3 rotationAxis = Vector3.forward;
+
+    #endregion
+
+    #region references
+
+    [Space(20f)]
+    [Header("References")]
+    public Animator walkAnimator;
+    public Transform ShootPosition;
+
+    #endregion
+
+    #region internal logic parameters
 
     [Space(20f)]
     [Header("Internal logic Parameter DON'T TOUCH")]
@@ -24,27 +39,17 @@ public class TestCharacterControler : MonoBehaviour
     public bool isMoving = false;
 
     public MovingStates currentMovingState = MovingStates.idle;
-    
-    private Animator _characterAnimator;
-    public Animator characterAnimator
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
     {
-        get
-        {
-            if (_characterAnimator == null)
-            {
-                _characterAnimator = GetComponent<Animator>();
-            }
-            return _characterAnimator;
-        }
+
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
         ManageInputs();
         ManageMovement();
@@ -52,18 +57,24 @@ public class TestCharacterControler : MonoBehaviour
 
     }
 
+    #region inputs
+
     private void ManageInputs()
     {
         walkingInput = Input.GetAxis("Horizontal");
         rotatingInput = Input.GetAxis("Vertical");
     }
 
+    #endregion
+
+    #region movements
+
     private void ManageMovement()
     {
-        if(walkingInput != 0)
+        if (walkingInput != 0)
         {
             EngageWalking();
-            if(rotatingInput != 0)
+            if (rotatingInput != 0)
             {
                 EngageRotation();
             }
@@ -73,11 +84,6 @@ public class TestCharacterControler : MonoBehaviour
         {
             isMoving = false;
         }
-    }
-
-    private void ManageAnimations()
-    {
-        characterAnimator.SetBool("isMoving", isMoving);
     }
 
     private void EngageRotation()
@@ -93,4 +99,15 @@ public class TestCharacterControler : MonoBehaviour
 
         transform.Translate(walkingVector);
     }
+
+    #endregion
+
+    #region Animations
+
+    private void ManageAnimations()
+    {
+        walkAnimator.SetBool("isMoving", isMoving);
+    }
+
+    #endregion
 }

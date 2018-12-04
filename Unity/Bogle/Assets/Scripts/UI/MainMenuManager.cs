bc0eec465748497467ea1c89c99cbf9cf1510bca
troleoleo <leolefebvre.contact@gmail.com>
2018-12-04 02:27:01 +0200

    Add Title Screen

---
+++ Unity/Bogle/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuManager : Singleton<MainMenuManager>
+{
+    public AudioSource MusicSource;
+
+    public AudioClip GameMusic;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (SceneManager.sceneCount > 1)
+        {
+            LaunchGame();
+        }
+        else
+        {
+            HealthDisplayManager.Instance.gameObject.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.anyKeyDown)
+        {
+            LaunchGame();
+        }
+	}
+
+    void LaunchGame()
+    {
+        MusicSource.clip = GameMusic;
+        MusicSource.Play();
+
+        GameManager.Instance.ReceivedRightToLaunchArenaMode();
+        HealthDisplayManager.Instance.gameObject.SetActive(true);
+
+        gameObject.SetActive(false);
+    }
+}
